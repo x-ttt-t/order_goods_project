5eb0652609d72383348da7fcbc249c7fd7fa59f7
Tao_Xiong <1198631307@qq.com>
2023-04-09 12:25:58 +0800

    config

---
+++ springcloud-config-server-3344/src/test/java/com/example/springcloudconfigserver3344/SpringcloudConfigServer3344ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigserver3344;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudConfigServer3344ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
